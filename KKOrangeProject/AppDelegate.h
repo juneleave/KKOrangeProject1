a164f48232ef4b869635f52c38ced90d44501eaa
world king <worldking@worlddeMacBook-Pro.local>
2019-09-27 17:31:12 +0800

    “firstCommit”

---
+++ KKOrangeProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKOrangeProject
+//
+//  Created by world king on 2019/9/27.
+//  Copyright © 2019 world king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKOrangeProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKOrangeProject
+//
+//  Created by world king on 2019/9/27.
+//  Copyright © 2019 world king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, copy) NSString *userId;
+@end
+
